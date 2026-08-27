db88251348847d9ee607cdc9af3100cb770913d9
Lap Dang Trung <Lap.Dang@aurecongroup.com>
2026-08-27 15:04:46 +0700

    update

--- ClassLibrary1/MyRibbon.vb
+++ ClassLibrary1/MyRibbon.vb
@@ -1,21 +1,11 @@
 ﻿Imports ExcelDna.Integration.CustomUI
-
-Imports System.IO
-Imports System.Reflection
+Imports System.Windows.Forms
 
 Public Class MyRibbon
     Inherits ExcelRibbon
 
     Public Overrides Function GetCustomUI(ribbonID As String) As String
-
-        Dim asm = Assembly.GetExecutingAssembly()
-
-        Using stream = asm.GetManifestResourceStream("PileUtilize.Ribbon.xml")
-            Using reader As New StreamReader(stream)
-                Return reader.ReadToEnd()
-            End Using
-        End Using
-
+        Return Nothing
     End Function
 
     Public Sub OnTestClick(control As IRibbonControl)
